2a1e7b7ca7bd12535d84d9876a0f1214c4fcb00a
logike-se <johan.westlund@logike.se>
2016-10-07 22:52:44 +0200

    fixed indentations

--- HWEmulated/include/memmgr.h
+++ HWEmulated/include/memmgr.h
@@ -18,23 +18,23 @@
 #include <string.h>
 
 typedef enum{
-	RAM_SIZE		= 0xFFFF,	//64k
-	ROM_KERNAL_SIZE	= 0x2000,	//8k
-	ROM_CHAR_SIZE	= 0x1000,	//4k
-	ROM_CART_SIZE	= 0x2000,	//8k
-	IO_MEM_SIZE		= 0x1000,	//4k
-	MEM_PAGE_SIZE	= 256
+	RAM_SIZE        = 0xFFFF,   //64k
+	ROM_KERNAL_SIZE = 0x2000,   //8k
+	ROM_CHAR_SIZE   = 0x1000,   //4k
+	ROM_CART_SIZE   = 0x2000,   //8k
+	IO_MEM_SIZE     = 0x1000,   //4k
+	MEM_PAGE_SIZE   = 256
 } E_MEMORY_SIZE;
 
 typedef enum{
-	BASE_RAM			= 0x0000,
-	BASE_KERNAL			= 0xE000,
-	BASE_CHAR			= 0xD000,
-	BASE_BASIC			= 0xB000,
-	BASE_CART_HI_GAME	= 0xE000,
-	BASE_CART_HI_EXT	= 0xA000,
-	BASE_CART_LO		= 0x8000,
-	BASE_IO				= 0xD000
+	BASE_RAM            = 0x0000,
+	BASE_KERNAL         = 0xE000,
+	BASE_CHAR           = 0xD000,
+	BASE_BASIC          = 0xB000,
+	BASE_CART_HI_GAME   = 0xE000,
+	BASE_CART_HI_EXT    = 0xA000,
+	BASE_CART_LO        = 0x8000,
+	BASE_IO             = 0xD000
 } E_MEM_BASE_ADDRESS;
 
 typedef enum{
@@ -49,29 +49,29 @@ typedef enum{
 
 /* Note: Real base address is IO_BASE_ADDRESS + MEM_BASE_ADDRESS */
 typedef enum{
-	BASE_VIC		= 0x000,
-	BASE_SID		= 0x400,
-	BASE_COLOR_RAM	= 0x800,
-	BASE_CIA1		= 0xC00,
-	BASE_CIA2		= 0xD00,
-	BASE_IO1		= 0xE00,
-	BASE_IO2		= 0xF00
+	BASE_VIC        = 0x000,
+	BASE_SID        = 0x400,
+	BASE_COLOR_RAM  = 0x800,
+	BASE_CIA1       = 0xC00,
+	BASE_CIA2       = 0xD00,
+	BASE_IO1        = 0xE00,
+	BASE_IO2        = 0xF00
 } E_IO_BASE_ADDRESS;
 
 typedef enum{
-	SIZE_VIC		= 0x400,
-	SIZE_SID		= 0x400,
-	SIZE_COLOR_RAM	= 0x400,
-	SIZE_CIA		= 0x100,
-	SIZE_IO			= 0x100
+	SIZE_VIC        = 0x400,
+	SIZE_SID        = 0x400,
+	SIZE_COLOR_RAM  = 0x400,
+	SIZE_CIA        = 0x100,
+	SIZE_IO         = 0x100
 } E_IO_SIZE;
 
 typedef enum{
-	LORAM 	= 0x01,
-	HIRAM 	= 0x02,
-	CHAREN	= 0x04,
-	GAME	= 0x08,
-	EXROM	= 0x10
+	LORAM   = 0x01,
+	HIRAM   = 0x02,
+	CHAREN  = 0x04,
+	GAME    = 0x08,
+	EXROM   = 0x10
 } E_BANK_SWITCH_MASK;
 
 typedef enum{
